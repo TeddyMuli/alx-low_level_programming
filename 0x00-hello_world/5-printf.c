6f58ca4c3fcafb6c551959af9fecc16d33c50240
TeddyMuli <tmuli974@gmail.com>
2022-12-08 13:33:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ * Description: C program that uses printf
+ *
+ * Return: Always 0 (SUCCESS)
+ *
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
