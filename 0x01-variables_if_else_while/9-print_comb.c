4ec968f66799d9bee703364f286e1c3ab31ce19c
TeddyMuli <tmuli974@gmail.com>
2022-12-14 15:57:13 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/**
+ * main - Entry point
+ * Description: Numbers
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int x = '0';
+	while (x <= '9')
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
