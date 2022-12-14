9b56b19005d23047cfd067d8de57d44bb1d54d5e
TeddyMuli <tmuli974@gmail.com>
2022-12-14 14:18:38 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet w/o q and e
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 'A';
+	while (x <= 'Z')
+	{
+		if (x != 'E' && x!= 'Q')
+		{
+			putchar(tolower(x));
+		}
+		else
+		{
+		}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
