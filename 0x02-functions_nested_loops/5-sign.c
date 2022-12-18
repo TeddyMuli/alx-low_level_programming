ff64a88af541b139da1fc065b21b9496c5f7b922
TeddyMuli <tmuli974@gmail.com>
2022-12-18 12:20:15 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
  * print_sign - Entry point
  * Description: prints the sign of a number
- * @c: interger
+ * @n: interger
  * Return: 1 if +, 0 if 0 ,-1 if -1
  */
 int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		return (1);
 	}
-	if (c == 0)
+	if (n == 0)
 	{
 		return (0);
 	}
