71e03bdda68ff9ad7ff01eabbc4552b30d334a67
TeddyMuli <tmuli974@gmail.com>
2022-12-18 06:30:18 -0800

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,19 +3,22 @@
  * main - Entry point
  * Description: print alphabet 10x
  * Return void
+ * print_alphabet_x10 - print alphabet 10x
  */
 void print_alphabet_x10(void)
 {
-	int x = 'a';
+	int x;
 	int i = 1;
 
 	while (i <= 10)
 	{
-		while (x <= 'z')
+		for (x = 'a'; x <= 'z'; x++)
 		{
 			_putchar(x);
-			x++;
 		}
+
+	_putchar('\n');
+
 	i++;
 	}
 }
