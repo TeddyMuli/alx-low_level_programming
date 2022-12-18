62e3bceb2d065066075b9000eba43e7c7c57bbce
TeddyMuli <tmuli974@gmail.com>
2022-12-18 12:17:24 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * Description: prints the sign of a number
+ * @c: interger
+ * Return: 1 if +, 0 if 0 ,-1 if -1
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		return (1);
+	}
+	if (c == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
