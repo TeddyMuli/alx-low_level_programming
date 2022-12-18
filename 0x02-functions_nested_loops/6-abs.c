b50fb81ec0cadfbf0dba66f8080468b6daacb07e
TeddyMuli <tmuli974@gmail.com>
2022-12-18 12:53:04 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs(int) - Entry point
+ * Description: Absolute value
+ * Return: 0
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
+}
+
