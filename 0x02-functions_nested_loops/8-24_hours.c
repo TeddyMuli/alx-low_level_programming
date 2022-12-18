357f0512ed463e368c49995077aa24f4fe11c395
TeddyMuli <tmuli974@gmail.com>
2022-12-18 13:46:45 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * void jack_bauer(void) - Entry point
+ * Description:  unction that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int x = 1;
+	while (x < 24)
+	{
+	for (int y = 1; y < 60; y++)
+	{
+	_putchar(i':'y);
+	}
+	x++;
+	}
+}
