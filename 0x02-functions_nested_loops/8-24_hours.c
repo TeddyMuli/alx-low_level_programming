e9f41595dcc41ab4f36deb2b718784a6759cdc55
TeddyMuli <tmuli974@gmail.com>
2022-12-18 14:35:19 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,40 @@
  */
 void jack_bauer(void)
 {
-	int x = 1;
-	while (x < 24)
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
+
+	hours_max = 58;
+	hours_tens = '0';
+
+	while (hours_tens < '3')
 	{
-	for (int y = 1; y < 60; y++)
+		if (hours_tens == '2')
+  			{											hours_max = '4';								}								hours_ones = '0';
+	while (hours_ones < hours_max)
 	{
-	_putchar(i':'y);
+
+ 		minutes_tens = '0';
+		while (minutes_tens < '6')
+		{
+			minutes_ones = '0';
+			while (minutes_ones < 58)
+			{
+														_putchar(hours_tens);
+                            _putchar(hours_ones);
+		            _putchar(':');
+ 			    _putchar(minutes_tens);
+         		    _putchar(minutes_ones);
+			    _putchar('\n');
+			    minutes_ones++;
+    			}
+			minutes_ones = '0';
+			minutes_tens++;
+		}
+		minutes_tens = '0';
+		hours_ones++;
 	}
-	x++;
-	}
+	hours_ones = '0';
+	hours_tens++;
+											}
 }
