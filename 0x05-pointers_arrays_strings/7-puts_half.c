0cf5596fdbf5dc31e4e57e6857675be4178db9f8
TeddyMuli <tmuli974@gmail.com>
2022-12-19 13:31:50 -0800

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: char to check
+ * Return: void
+ */
+
+void puts_half(char *str)
+
+{
+	int num = 0, i;
+	
+	while (str[num] != '\0')
+		num++;
+	if (num + 1 % 2 != '0')
+		i = (num - 1) / 2;
+	else
+		i = (num / 2);
+	i++;
+	
+	for (num = i; str[num] != '\0'; num++)
+	_putchar(str[num]);
+	_putchar('\n');
+
+}
