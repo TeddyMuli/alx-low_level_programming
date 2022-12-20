83d7eeaf198b9a740802e0c2510d0a74af31bf3c
TeddyMuli <tmuli974@gmail.com>
2022-12-20 14:30:18 -0800

    14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers- entry point
+ * Return: void
+ */
+
+int x = '0';
+
+for (int i = 0; i <= 9; i++)
+{
+	while (x <= "13")
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
