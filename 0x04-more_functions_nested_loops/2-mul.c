71a3b6810523a6fed95bd513d7085f2d0f0ab44c
TeddyMuli <tmuli974@gmail.com>
2022-12-20 13:41:59 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * mul- Entry point
+ * @a: integer
+ * @b: integer
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int i = a * b;
+	return (i);	
+}
