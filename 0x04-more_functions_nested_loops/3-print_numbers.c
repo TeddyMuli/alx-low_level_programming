ebdf015762a4e19c13406b35c4982ef64dcbd4f1
TeddyMuli <tmuli974@gmail.com>
2022-12-20 14:02:39 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- Entry point
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
