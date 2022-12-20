600a0e02335d7ce541d4dbb7dc27cc9cf1656305
TeddyMuli <tmuli974@gmail.com>
2022-12-20 14:22:28 -0800

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,12 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while((i <= 9) && i != 2 && i != 4)
+	while(i <= 9)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 		i++;
 	}
 	_putchar('\n');
