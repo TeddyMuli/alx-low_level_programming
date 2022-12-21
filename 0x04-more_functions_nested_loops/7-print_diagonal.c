b7e1b06f076cddf248eb22ad384983dbde8c2821
TeddyMuli <tmuli974@gmail.com>
2022-12-20 16:00:44 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal- entry point
+ * Return: void
+ * @n: integer
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			while (j < i)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+
+			i++;
+		}
+	}
+}
