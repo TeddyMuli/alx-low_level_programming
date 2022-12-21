17380588dad11b5e6fad9593e1b6fc9cd63925ed
TeddyMuli <tmuli974@gmail.com>
2022-12-20 16:25:08 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,17 @@ void print_diagonal(int n)
 	int i = 0;
 	int j = 0;
 
-	while (i < n && n >0)
+	while (i < n && n > 0)
 	{
 		while (j < i)
 		{
 			_putchar(' ');
+			j++;
 		}
+
 		_putchar('\\');
 		_putchar('\n');
-
-			i++;
+		i++;
 	}
 	if (i == 0)
 		_putchar('\n');
