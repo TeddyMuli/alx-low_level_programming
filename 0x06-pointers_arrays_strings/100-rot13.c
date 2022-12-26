98eca830007fde3861d4d6d7421fd5f80f555253
TeddyMuli <tmuli974@gmail.com>
2022-12-26 05:07:31 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
 
-void rot13(char* s)
+/**
+ * rot13- encodes a  string
+ * @s: string
+ * return: address bof s
+ */
+
+char *rot13(char *s)
 {
-	int i = 0;
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (i != '\0')
-	{
-	if (isalpha(s[i]))
-       	{
-     		 s[i] = (isupper(s[i]) ? 'A' : 'a') + (s[i] - (isupper(s[i]) ? 'A' : 'a') + 13) % 26;
-	}
-  }
-	i++;
+for (i = 0; *(s + i); i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (a[j] == *(s + i))
+	*(s + i)  = b[j];
+	break;
+}
+}
+return (s);
 }
