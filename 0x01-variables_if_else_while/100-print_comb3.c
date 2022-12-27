ac95e9d8f91e20603dafcbabe477559541c70dad
TeddyMuli <tmuli974@gmail.com>
2022-12-27 03:37:58 -0800

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+ 	    int i = 0;
+        	int j;
+
+	    while(i <= 9)
+	        {
+            for (j = 0; j <= 9; j++)
+               {
+                       if (i < j)
+	          {
+										                                  _putchar('i');
+		                  _putchar('j');
+		                    if (i != 8)
+		                             {
+	                                 _putchar(',');
+		                        _putchar(' ');
+                                 }
+												                         }
+		            }
+
+                  i++;
+          }
+
+}
+
+
