35e790152ffa9ea45c951275fdcfd2c37ab23ed8
TeddyMuli <tmuli974@gmail.com>
2022-12-27 10:09:52 -0800

    advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,8 +31,12 @@ if (n <= 15 && n >= 0)
 		{
 			_putchar((prod / 100) + 48);
 			_putchar((prod / 10) % 10 + 48);
-		} else if (prod <= 99 && prod >= 10)
+		}
+	       	else if (prod <= 99 && prod >= 10)
+		{
 			_putchar((prod / 10) + 48);
 			_putchar((prod % 10) + 48);
-		}										_putchar('\n');									}
+		}
+			_putchar('\n');								}
+		}
 }
