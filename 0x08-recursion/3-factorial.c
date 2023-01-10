1f6bf46cf975d37b6151792edee08f116b084eb5
TeddyMuli <tmuli974@gmail.com>
2023-01-10 06:14:27 -0800

    fac

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <math.h>
 
+/**
+ * factorial - entry point
+ * @n: integer
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+	 return (1);
+	}
+	else 
+	        return pow(factorial(n), factorial(n - 1));
+}
