83d118cd32385f6d468586f8fb242954f5e1704a
TeddyMuli <tmuli974@gmail.com>
2023-01-10 05:13:33 -0800

    another

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	if(*s != '\0')
+	length = 0;
+
+	if(*s)
 	{
-	return _strlen_recursion(s + 1);
+		length++;
+		length += _strlen_recursion;
 	}
-	return _strlen_recursion;
+	return length;
 }
