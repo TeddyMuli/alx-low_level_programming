23e6962c155b50d2b95a2c21814fd40fda9c3f4d
TeddyMuli <tmuli974@gmail.com>
2023-01-11 09:00:54 -0800

    new

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+/**
+**_memcpy - entry point
+*@src:char
+*@dest:dest
+*@n:int
+*return: char
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
