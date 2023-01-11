9cb9bf86862da94e422061ba09224c9e15a43eb2
TeddyMuli <tmuli974@gmail.com>
2023-01-11 12:15:10 -0800

    new

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+* _strspn - entry
+* @s: string
+* @accept: string
+* return: int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int length = 0;
+unsigned int len1 = strlen(s);
+unsigned int len2 = strlen(accept);
+int i = 0;
+
+while (i < len1)
+{
+	bool match = false;
+	for (int j = 0; j < len2; j++)
+	{
+		if (accept[j] == s[i])
+		{
+			match = true;
+			break;
+		}
+	if (!match) break;
+	else length++;
+	i++;
+}
+}
+}
