3c650d7c4beafc8aa8f95c8dc9698864bd7ca064
TeddyMuli <tmuli974@gmail.com>
2023-01-12 08:03:54 -0800

    new

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * *create_array - entry
+ * @size: int
+ * @c: char
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr[];
+	unsigned int n;
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (n = 0; n < size; n++)
+		arr[size] = c;
+	return (arr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strdup - entry
+ * @str: string
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	char *array = (char*)malloc (20 * sizeof(char))
+	for (int i = 0; i < 20; i++)
+	{
+		str = arr[i];		
+	}
+	return (str);
+}
