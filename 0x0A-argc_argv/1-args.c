68878843cfe92242372c5a739d6743fc5104e447
TeddyMuli <tmuli974@gmail.com>
2023-01-16 08:32:33 -0800

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: member
+ * @argv: member
+ * Return: main
+ */
+int main(int argc, char *argv)
+{
+	while (argc--)
+		printf ("%d\n", argc++)
+	return (0);
+}
