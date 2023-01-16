d08d952c1b995350cc8fa2fa798362829a180d07
TeddyMuli <tmuli974@gmail.com>
2023-01-16 09:12:37 -0800

    new

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -15,8 +15,10 @@ int main(int argc, char *argv[])
 		printf("%d\n", argc - 1);
 	else
 	{
-		for (i = 0; *argv; i++, argv++);
-			printf("%d\n", i - 1);
+		for (i = 0; *argv; i++, argv++)
+			;
+		
+		printf("%d\n", i - 1);
 	}
 	return (0);
 }
