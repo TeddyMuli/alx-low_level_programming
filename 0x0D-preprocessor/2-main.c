63bc69d622e9ed3d675799ca0f81a4e20f2e201e
TeddyMuli <tmuli974@gmail.com>
2023-01-17 10:13:17 -0800

    new

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: member
+ * @argv: member
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
