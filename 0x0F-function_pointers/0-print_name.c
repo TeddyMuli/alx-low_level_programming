6d17b95441d73bd1901537a01aedf9bab95ff696
TeddyMuli <tmuli974@gmail.com>
2023-01-22 22:34:58 -0800

    new

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * @name: mome
+ * @f: somethung
+ * print_name - entry oiny
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	_putchar('T');
+	_putchar('e');
+	_putchar('d');
+}
