25b4a133e17e9edcdc1cbb0341b67f1e017bdf2d
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-01-24 16:36:54 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,12 +22,12 @@ int (*get_op_func(char *s))(int, int)
 };
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{	
-		if (strcmp(s, ops[i].op) == 0)
+		if (!strcmp(ops[i].op, s))
 			return (ops[i].f);
 		
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
