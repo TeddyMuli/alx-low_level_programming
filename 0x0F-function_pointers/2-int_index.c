a74fc7ee729d7ce5dd4ddb8219bf6937d326efd8
TeddyMuli <tmuli974@gmail.com>
2023-01-23 21:58:27 -0800

    new

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * int_index - entry point
+ * int_index - entry poiint
  * @array: the array
- * @size: size of the array
- * @int: the integer
+ * @size: number of elements
+ * @int: value to be compared
  * Return: int
  */
 
@@ -12,14 +12,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size < 0 || size = 0)
-		return (-1);
-	else
-		while (i < size)
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
 		{
-		if (cmp(array[i]))
-			return (i);
+			while(i < size)
+			{
+				if (cmp(array[i])
+					return (-1);
 
-		i++;
+				i++;
+			}
 		}
+	}
+	else
+		return (-1);
 }
