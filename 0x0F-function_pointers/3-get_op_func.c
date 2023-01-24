67acc8d1396afaf74e9489d687a6ae71a6e09d4e
TeddyMuli <tmuli974@gmail.com>
2023-01-24 14:13:24 -0800

    new

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,12 +22,9 @@ int (*get_op_func(char *s))(int, int)
 };
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{	
-		if (!strcmp(ops[i].op, s))
-			return (ops[i].f);
-		
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,33 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-	int (*opt)(int, int);
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-if (
-(strcmp(argv[2], "+") &&
-strcmp(argv[2], "-") &&
-strcmp(argv[2], "*") &&
-strcmp(argv[2], "/") &&
-strcmp(argv[2], "%"))
-)
+
+	oprt = get_op_func(argv[2]);
+
+if (!oprt)
 {
 printf("Error\n");
 exit(99);
 }
-
-if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-&& atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
