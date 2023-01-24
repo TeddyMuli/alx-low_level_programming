23b9cbdef4ff71e5ab37d173bf6e081d79f7b0f3
TeddyMuli <tmuli974@gmail.com>
2023-01-24 13:54:46 -0800

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,17 +7,19 @@
  * @age: integer
  * @owner: integer
  */
-typedef struct dog 
+struct dog 
 {
 	char *name;
 	char *owner;
 	float age;
-} my_dog;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-my_dog *new_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
