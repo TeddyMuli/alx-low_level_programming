8531666b1b3d8c8770989bb269e50f9b6220ba9d
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-11 13:04:52 +0300

    changed first file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,42 @@
 */
 
 unsigned int binary_to_uint(const char *b) {
-    unsigned int x = *b;
-    unsigned int final = 0;
-    unsigned int size = sizeof(*b);
-    for (int n = 0; n < size; n++ )
+    unsigned int dec = 0, i = 0, rem;
+    unsigned int binary = atoi(b);
+    unsigned int length = sizeof(binary);
+    for (int n = 0; n < binary; n++)
     {
-       x = x * pow(2,size);
-       final = final + x;
+      if ((b + n) != "0" || (b + n) != "1")
+            printf("Enter valid binary number\n");
+        
+        else
+        {
+            while (binary != 0)
+            {
+             rem = binary % 10;
+             binary = binary / 10;
+             dec += rem * pow(2,i);
+             i++;
+            }
+        }
+      //  return (dec);
     }
-    return (final);
+}
+
+int main(void)
+{
+    unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+   /* n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    */
+    return (0);
 }
