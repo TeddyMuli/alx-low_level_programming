7607210942c86ecc1fa88de6871adbc493c104f3
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-12 18:42:00 +0300

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * print_list - entry point
+ * @h: member
+ * return: size_t
+ */
+
+size_t print_list(const list_t *h) {
+    int length = 0;
+     while (h != NULL)
+     {
+        /* code */
+        printf("%d",h->str);
+        h = h->next;
+        length++;
+     }
+  return (length);    
+}
