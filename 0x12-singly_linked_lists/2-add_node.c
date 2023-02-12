f7721ae45f9da8669d4fd107104ede6eadee4d5d
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-12 22:22:48 +0300

    change

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,9 +3,30 @@
 #include "lists.h"
 
 list_t *add_node(list_t **head, const char *str) {
-    list_t *new_node = malloc(sizeof(list_t));
+    list_t *new_node;
+    char *dup;
+    int len;
 
+    new_node = malloc(sizeof(list_t));
+    if (new_node = NULL)
+        return (NULL);
+
+    dup = strdup(str);
+    if(dup = NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    for(len = 0; str[len];)
+    {
+        len++;
+    }
+    new_node->str = dup;
+    new_node->len = len;
     new_node->next = *head;
+    *head = new_node;
+
 
     head = new_node;
 }
