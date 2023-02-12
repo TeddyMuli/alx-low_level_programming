42afaa7ce41972fc8d9caf9584599a2531c09c49
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-12 19:27:34 +0300

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+ * list_len - entry point
+ * @h: member
+ * return: int
+ */
+size_t list_len(const list_t *h) {
+    size_t length_of_list = 0;
+
+    while (h != NULL)
+    {
+        /* code */
+        length_of_list++;
+        h = h->next;
+    }
+    return (length_of_list);
+}
