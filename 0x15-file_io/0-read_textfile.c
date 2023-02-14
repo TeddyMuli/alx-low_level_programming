c6e6926df1405e271bad939258eeb583c01fddc5
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-14 10:32:56 +0300

    new

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     if(filename == NULL)
         return (0);
     
-    buffer = malloc(sizeof(letters));
+    buffer = malloc(sizeof(char) * letters);
     if(buffer == NULL)
         return (0);
 
