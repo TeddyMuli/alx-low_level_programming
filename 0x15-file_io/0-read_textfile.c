c40bb52a9167e4cb760261fbf63a07518ab0b5cd
TeddyMuli <113882483+TeddyMuli@users.noreply.github.com>
2023-02-14 09:25:37 +0300

    new

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,8 @@
  * @letters: member
  * return: size_t
 */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters) 
+{
     ssize_t o, r, w;
     char *buffer;
     
