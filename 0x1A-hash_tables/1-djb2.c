9d49903c0dbab4f38f3ac69d9192542300aef86e
TeddyMuli <tmuli974@gmail.com>
2023-05-08 08:24:00 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "hash_tables.h"
-
+/*
+* hash_djb2 - function
+* @str: parameter
+* Return: hash
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
