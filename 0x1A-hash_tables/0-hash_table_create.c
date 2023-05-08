92a1108c1c847216e4c2862553c3c303c64ffeb3
TeddyMuli <tmuli974@gmail.com>
2023-05-08 07:56:57 +0300

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,11 +3,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+    unsigned long int i;
+    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return NULL;
 
-
-    hash_table->array = (unsigned long int *)malloc(size * sizeof(unsigned long int));
-    
+    hash_table->array = malloc(size * sizeof(unsigned long int));
     hash_table->size = size;
+    if (hash_table->array == NULL)
+        return NULL;
+    for (i = 0; i < size; i++)
+        hash_table->array[i] = NULL;
+
     return hash_table;
 }
